fa8b30859b93a2bf9b2e486e8f6ee8e245fa7592
Ade Ridwan <sukun024@gmail.com>
2023-11-09 03:03:40 +0700

    memperbaiki take photo

--- app/Models/siswa.php
+++ app/Models/siswa.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class siswa extends Authenticatable
+class Siswa extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
