2a8d87bca3fcb368637da214446d1149de2a32b0
Ade Ridwan <sukun024@gmail.com>
2023-11-10 10:07:22 +0700

    add button logout

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -6,8 +6,8 @@
                 <img src="assets/img/sample/avatar/avatar1.jpg" alt="avatar" class="imaged w64 rounded">
             </div>
             <div id="user-info">
-                <h2 id="user-name">{{auth('siswa')->user()->nama_lengkap}}</h2>
-                <span id="user-role">{{auth('siswa')->user()->kelas}}</span>
+                <h2 id="user-name">{{ auth('siswa')->user()->nama_lengkap }}</h2>
+                <span id="user-role">{{ auth('siswa')->user()->kelas }}</span>
             </div>
 
         </div>
@@ -25,16 +25,6 @@
                         </div>
                         <div class="menu-name">
                             <span class="text-center">Profil</span>
-                        </div>
-                    </div>
-                    <div class="item-menu text-center">
-                        <div class="menu-icon">
-                            <a href="" class="danger" style="font-size: 40px;">
-                                <ion-icon name="calendar-number"></ion-icon>
-                            </a>
-                        </div>
-                        <div class="menu-name">
-                            <span class="text-center">Cuti</span>
                         </div>
                     </div>
                     <div class="item-menu text-center">
@@ -57,6 +47,16 @@
                             Lokasi
                         </div>
                     </div>
+                    <div class="item-menu text-center">
+                        <div class="menu-icon">
+                            <a href="/proseslogout" class="danger" style="font-size: 40px;">
+                                <ion-icon name="log-out"></ion-icon>
+                            </a>
+                        </div>
+                        <div class="menu-name">
+                            <span class="text-center">Keluar</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -70,12 +70,12 @@
                             <div class="presencecontent">
                                 <div class="iconpresence">
                                     @if ($presensihariini != null)
-                                    @php
-                                        $path = Storage::url('uploads/absensi/'. $presensihariini->foto_in);
-                                    @endphp
-                                        <img src="{{url($path)}}" alt="" class="imaged w64">
+                                        @php
+                                            $path = Storage::url('uploads/absensi/' . $presensihariini->foto_in);
+                                        @endphp
+                                        <img src="{{ url($path) }}" alt="" class="imaged w64">
                                     @else
-                                    <ion-icon name="camera"></ion-icon>
+                                        <ion-icon name="camera"></ion-icon>
                                     @endif
                                 </div>
                                 <div class="presencedetail">
@@ -92,12 +92,12 @@
                             <div class="presencecontent">
                                 <div class="iconpresence">
                                     @if ($presensihariini?->jam_out)
-                                    @php
-                                        $path = Storage::url('uploads/absensi/'. $presensihariini->foto_out);
-                                    @endphp
-                                        <img src="{{url($path)}}" alt="" class="imaged w64">
+                                        @php
+                                            $path = Storage::url('uploads/absensi/' . $presensihariini->foto_out);
+                                        @endphp
+                                        <img src="{{ url($path) }}" alt="" class="imaged w64">
                                     @else
-                                    <ion-icon name="camera"></ion-icon>
+                                        <ion-icon name="camera"></ion-icon>
                                     @endif
                                 </div>
                                 <div class="presencedetail">
@@ -112,13 +112,15 @@
         </div>
 
         <div id="rekappresensi">
-            <h4>Rekap Presensi Bulan {{$namabulan[$bulanini]}} Tahun {{$tahunini}}</h4>
+            <h4>Rekap Presensi Bulan {{ $namabulan[$bulanini] }} Tahun {{ $tahunini }}</h4>
             <div class="row">
                 <div class="col-3">
                     <div class="card">
                         <div class="card-body text-center" style="padding: 16px 12px !important">
-                            <span class="badge bg-danger" style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">{{$rekappresensi->jmlhadir}}</span>
-                            <ion-icon name="accessibility-outline" style="font-size: 1.6rem;" class="text-primary"></ion-icon><br>
+                            <span class="badge bg-danger"
+                                style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">{{ $rekappresensi->jmlhadir }}</span>
+                            <ion-icon name="accessibility-outline" style="font-size: 1.6rem;"
+                                class="text-primary"></ion-icon><br>
                             <span>Hadir</span>
                         </div>
                     </div>
@@ -126,8 +128,10 @@
                 <div class="col-3">
                     <div class="card">
                         <div class="card-body text-center" style="padding: 16px 12px !important">
-                            <span class="badge bg-danger" style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">0</span>
-                            <ion-icon name="newspaper-outline" style="font-size: 1.6rem;" class="text-success"></ion-icon><br>
+                            <span class="badge bg-danger"
+                                style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">0</span>
+                            <ion-icon name="newspaper-outline" style="font-size: 1.6rem;"
+                                class="text-success"></ion-icon><br>
                             <span>Izin</span>
                         </div>
                     </div>
@@ -135,7 +139,8 @@
                 <div class="col-3">
                     <div class="card">
                         <div class="card-body text-center" style="padding: 16px 12px !important">
-                            <span class="badge bg-danger" style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">0</span>
+                            <span class="badge bg-danger"
+                                style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">0</span>
                             <ion-icon name="medkit-outline" style="font-size: 1.6rem;" class="text-warning"></ion-icon><br>
                             <span>Sakit</span>
                         </div>
@@ -144,7 +149,8 @@
                 <div class="col-3">
                     <div class="card">
                         <div class="card-body text-center" style="padding: 16px 12px !important">
-                            <span class="badge bg-danger" style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">{{$rekappresensi->jmlterlambat}}</span>
+                            <span class="badge bg-danger"
+                                style="position: absolute; top:3px; right:10px; font-size:0.6rem; z-index:999">{{ $rekappresensi->jmlterlambat }}</span>
                             <ion-icon name="alarm-outline" style="font-size: 1.6rem;" class="text-danger"></ion-icon><br>
                             <span>Telat</span>
                         </div>
@@ -172,40 +178,42 @@
                 <div class="tab-pane fade show active" id="home" role="tabpanel">
                     <ul class="listview image-listview">
                         @foreach ($historibulanini as $d)
-                        @php
-                            $path = Storage::url('uploads/absensi/'.$d->foto_in);
-                        @endphp
-                        <li>
-                            <div class="item">
-                                <div class="icon-box bg-primary">
-                                    <ion-icon name="calendar-outline" role="img" class="md hydrated"
-                                        aria-label="calendar outline"></ion-icon>
+                            @php
+                                $path = Storage::url('uploads/absensi/' . $d->foto_in);
+                            @endphp
+                            <li>
+                                <div class="item">
+                                    <div class="icon-box bg-primary">
+                                        <ion-icon name="calendar-outline" role="img" class="md hydrated"
+                                            aria-label="calendar outline"></ion-icon>
+                                    </div>
+                                    <div class="in">
+                                        <div>{{ date('d-m-Y', strtotime($d->tgl_presensi)) }}</div>
+                                        <span class="badge badge-success">{{ $d->jam_in }}</span>
+                                        <span
+                                            class="badge badge-danger">{{ $d?->jam_out ? $d->jam_out : 'Belum Absen' }}</span>
+                                    </div>
                                 </div>
-                                <div class="in">
-                                    <div>{{date("d-m-Y",strtotime($d->tgl_presensi))}}</div>
-                                    <span class="badge badge-success">{{$d->jam_in}}</span>
-                                    <span class="badge badge-danger">{{$d?->jam_out ? $d->jam_out : 'Belum Absen'}}</span>
-                                </div>
-                            </div>
-                        </li>
+                            </li>
                         @endforeach
                     </ul>
                 </div>
                 <div class="tab-pane fade" id="profile" role="tabpanel">
                     <ul class="listview image-listview">
                         @foreach ($leaderboard as $d)
-                        <li>
-                            <div class="item">
-                                <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="image">
-                                <div class="in">
-                                    <div>
-                                        <b>{{$d->nama_lengkap}}</b><br>
-                                        <small class="text-muted">{{$d->kelas}}</small>
+                            <li>
+                                <div class="item">
+                                    <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="image">
+                                    <div class="in">
+                                        <div>
+                                            <b>{{ $d->nama_lengkap }}</b><br>
+                                            <small class="text-muted">{{ $d->kelas }}</small>
+                                        </div>
+                                        <span
+                                            class="badge {{ $d->jam_in < '07:00' ? 'bg-success' : 'bg-danger' }}">{{ $d->jam_in }}</span>
                                     </div>
-                                    <span class="badge {{$d->jam_in < "07:00" ? "bg-success" : "bg-danger"}}">{{$d->jam_in}}</span>
                                 </div>
-                            </div>
-                        </li>
+                            </li>
                         @endforeach
 
                     </ul>
